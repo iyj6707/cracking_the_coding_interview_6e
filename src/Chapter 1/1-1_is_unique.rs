2e118e39be7d8f6bde1467021b6d1b0c6691dc45
iyj <iyj6707@naver.com>
2020-03-01 19:21:30 +0900

    Solve 1-1 problem

---
+++ src/Chapter 1/1-1_is_unique.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+
+fn check_unique_with_data_structure(word: &str) -> bool {
+    let mut chs = HashSet::new();
+    for ch in word.chars() {
+        if chs.contains(&ch) {
+            return true;
+        } else {
+            chs.insert(ch);
+        }
+    }
+    false
+}
+
+fn check_unique_without_data_structure(word: &str) -> bool {
+    let mut bit_vec: u128 = 0;
+    let mut ch_num: u128;
+    for ch in word.to_lowercase().chars() {
+        ch_num = ch as u128 - 'a' as u128;
+        if bit_vec & (1 << ch_num) > 0 {
+            return true;
+        }
+        bit_vec |= 1 << ch_num;
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::check_unique_with_data_structure;
+    use crate::check_unique_without_data_structure;
+
+    #[test]
+    fn test_with_data_structure() {
+        assert_eq!(check_unique_with_data_structure("yellow"), true);
+        assert_eq!(check_unique_with_data_structure("brown"), false);
+    }
+
+    #[test]
+    fn test_without_data_structure() {
+        assert_eq!(check_unique_without_data_structure("purple"), true);
+        assert_eq!(check_unique_without_data_structure("blue"), false);
+    }
+}
+
+fn main() {
+    check_unique_with_data_structure("hello");
+    check_unique_without_data_structure("hello");
+}
