2cc3ca54e70cc3162817ee5933e9c1629b9ed31a
iyj <iyj6707@naver.com>
2020-03-03 23:52:59 +0900

    Edit 1-2 problems

--- src/Chapter 1/1-2_is_permutation.rs
+++ src/Chapter 1/1-2_is_permutation.rs
@@ -20,6 +20,14 @@ fn is_permutation(s1: &str, s2: &str) -> bool {
     true
 }
 
+fn is_permutation_sort(s1: &str, s2: &str) -> bool {
+    let mut s1_vec: Vec<char> = s1.chars().collect();
+    let mut s2_vec: Vec<char> = s2.chars().collect();
+    s1_vec.sort();
+    s2_vec.sort();
+    s1_vec == s2_vec
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -31,8 +39,17 @@ mod test {
         assert_eq!(is_permutation("abbcde", "bcbdea"), true);
         assert_eq!(is_permutation("abcde", "abcd"), false);
     }
+
+    #[test]
+    fn check_permutation_sort() {
+        assert_eq!(is_permutation_sort("abcd", "acbd"), true);
+        assert_eq!(is_permutation_sort("abcde", "aebcc"), false);
+        assert_eq!(is_permutation_sort("abbcde", "bcbdea"), true);
+        assert_eq!(is_permutation_sort("abcde", "abcd"), false);
+    }
 }
 
 fn main() {
     is_permutation("abcd", "dcba");
+    is_permutation_sort("acdb", "bacd");
 }
